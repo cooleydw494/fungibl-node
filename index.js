40c8b5bb475f302bd434d791db5dd307cc8c1a0a
cooleydw494 <cooleydw494@gmail.com>
2022-12-13 16:16:16 -0500

    stubbed out nodejs server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express');
+const reach = require('@reach-sh/stdlib');
 const app = express();
 const actualBearerToken = process.env.BEARER_TOKEN;
 
@@ -11,6 +12,45 @@ app.get('/do-thing', checkBearerToken, (req, res) => {
 app.listen(3000, () => {
     console.log('Fungibl API server listening on port 3000');
 });
+
+let SpecifyBuyerToContract = async (ctcInfoStr, buyer) => {
+    return new Promise(async (resolve) => {
+        try {
+            console.log("setting buyer.....");
+
+            const ctcInfo = JSON.parse(ctcInfoStr);
+
+            const ctc = CreatorAccount.contract(backend, ctcInfo);
+            const call = async (f) => {
+                let res = undefined;
+                try {
+                    res = await f();
+                } catch (e) {
+                    res = [`err`, e];
+                    if (res !== null) {
+                        console.log("Failed to bind buyer to contract..", e);
+                        resolve(false);
+                    }
+                }
+                console.log(`res`, res);
+            };
+
+            try {
+                const Creator = ctc.a.Creator;
+                await call(() => Creator.setBuyer(reach.formatAddress(buyer)));
+
+                console.log(`buyer ${buyer} specified for contract ${ctcInfoStr}...`);
+                resolve(true);
+            } catch (e) {
+                console.log("Failed to bind buyer to contract..", e);
+                resolve(false);
+            }
+        } catch (e) {
+            console.log("Failed to bind buyer to contract..", e);
+            resolve(false);
+        }
+    });
+}
 
 function checkBearerToken(req, res, next) {
     const bearerHeader = req.headers['authorization'];
